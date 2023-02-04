92b027ee7e3b91da8117b9b4c9b25e6b32cb2b9e
Dzmitry Struneuski <dzmitry_struneuski@epam.com>
2023-02-04 15:03:16 +0300

    learn conditional

--- Ch2 - Basics/conditionals_start.py
+++ Ch2 - Basics/conditionals_start.py
@@ -5,15 +5,35 @@
 
 
 
+from unittest import case
+
+
 def main():
-    x, y = 10, 100
+    x, y = 1070, 100
 
     # conditional flow uses if, elif, else
-
+    if x < y:
+        result1 = "x lower than y"
+    elif x == y:
+        result1 = "x equals y"
+    else:
+        result1 = "x greater than y"
+    print(result1) 
     # conditional statements let you use "a if C else b"
-
+    result2 = print ("x lower than y") if x < y else "x equals or greater than y"
+    print(result2)
     # match-case makes it easy to compare multiple values
-    value = "one"
+    value = "5"
+    match value:
+        case "one":
+            result3 = 1
+        case "two":
+            result3 = 2
+        case "three" | "four":
+            result3 = (3,4)
+        case _:
+            result3 = "Another match"
+    print(result3)
 
 if __name__ == "__main__":
     main()
