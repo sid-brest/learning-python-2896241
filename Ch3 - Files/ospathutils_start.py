14508c19606a2eeb4434f0112cb0331942bf0e5a
Dzmitry Struneuski <dzmitry_struneuski@epam.com>
2023-02-04 22:29:01 +0300

    ospathutils learn

--- Ch3 - Files/ospathutils_start.py
+++ Ch3 - Files/ospathutils_start.py
@@ -12,18 +12,25 @@ import time
 
 def main():
     # Print the name of the OS
-
+    print(os.name)
     
     # Check for item existence and type
+    print("Exist testfile " + str(path.exists("testfile.txt")))
+    print("Exist file " + str(path.isfile("testfile.txt")))
+    print("Exist clear dir " + str(path.isdir("testfile.txt")))
 
     
     # Work with file paths
+    print("File path is " + str(path.realpath("testfile.txt")))
 
     
     # Get the modification time
+    print("Modification time is " + time.ctime(path.getctime("testfile.txt")))
 
     
     # Calculate how long ago the item was modified
+    timediff = datetime.datetime.now() - datetime.datetime.fromtimestamp(path.getmtime("testfile.txt"))
+    print("It has been " + str(timediff) + " since the file was modified")
 
   
 if __name__ == "__main__":
