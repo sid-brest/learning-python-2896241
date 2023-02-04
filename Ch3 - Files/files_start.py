9ec14bf40f291c3c7c9c17df56a848fee58eb784
Dzmitry Struneuski <dzmitry_struneuski@epam.com>
2023-02-04 19:50:44 +0300

    learn files

--- Ch3 - Files/files_start.py
+++ Ch3 - Files/files_start.py
@@ -6,19 +6,26 @@
 
 def main():  
     # Open a file for writing and create it if it doesn't exist
-
-    
-    # Open the file for appending text to the end
-
-
+    file1 = open("testfile.txt", "w+")
+        
     # write some lines of data to the file
-
+    for i in range(10):
+        file1.write("This is a text %d\n" % (i+1))
     
     # close the file when done
-
+    file1.close
     
-    # Open the file back up and read the contents
+    # Open the file for appending text to the end
+    file1 = open("testfile.txt", "a")
+    for i in range(10):
+        file1.write("This is a new text %d\n" % (i+1))
+    file1.close
 
+    # Open the file back up and read the contents
+    file1 = open("testfile.txt", "r")
+    if file1.mode == 'r':
+        content = file1.read()
+    print(content)
     
 if __name__ == "__main__":
     main()
