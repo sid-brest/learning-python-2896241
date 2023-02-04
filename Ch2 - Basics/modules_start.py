492188c765535d1d089583d7674efa57ee60ff27
Dzmitry Struneuski <dzmitry_struneuski@epam.com>
2023-02-04 18:02:35 +0300

    learn modules

--- Ch2 - Basics/modules_start.py
+++ Ch2 - Basics/modules_start.py
@@ -3,12 +3,12 @@
 
 
 # TODO: import the math module, which contains features for working with mathematics
-
+import math
 
 # TODO: the math module contains lots of pre-built functions
-
+print(math.sqrt(25))
 
 # TODO: in addition to functions, some modules contain useful constants 
-
+print(math.pi)
 
 # TODO: try some of the math functions for yourself here:
