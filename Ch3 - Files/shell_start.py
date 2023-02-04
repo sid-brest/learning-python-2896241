e54a552c133b8f5c0f363f43aec8ec43caf0729b
Dzmitry Struneuski <dzmitry_struneuski@epam.com>
2023-02-05 00:33:19 +0300

    learn shell

--- Ch3 - Files/shell_start.py
+++ Ch3 - Files/shell_start.py
@@ -5,19 +5,27 @@
 
 import os
 from os import path
+import shutil
+from shutil import make_archive
+from zipfile import ZipFile
 
 def main():
     # make a duplicate of an existing file
-    if path.exists("textfile.txt"):
+    if path.exists("testfile.txt"):
         # get the path to the file in the current directory
-        
+        source = path.realpath("testfile.txt")
         # let's make a backup copy by appending "bak" to the name
-        
+        destination = source + ".bak"
         # rename the original file
-        
+        shutil.copy(source,destination)
+        os.rename("testfile.txt", "newfile.txt")
         # now put things into a ZIP archive
-
+        # root_dir,tail = path.split(source)
+        # shutil.make_archive("archive", "zip", root_dir)
         # more fine-grained control over ZIP files
+        with ZipFile("testfile.zip","w") as newzip:
+            newzip.write("newfile.txt")
+            newzip.write("testfile.txt.bak")
 
       
 if __name__ == "__main__":
