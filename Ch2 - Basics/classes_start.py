d3603a8a9a6c475ba3a66fb3d1b24b975de8fd0e
Dzmitry Struneuski <dzmitry_struneuski@epam.com>
2023-02-04 17:57:39 +0300

    learn classes

--- Ch2 - Basics/classes_start.py
+++ Ch2 - Basics/classes_start.py
@@ -2,4 +2,29 @@
 # Example file for working with classes
 # LinkedIn Learning Python course by Joe Marini
 #
+class Vehicle():
+    def __init__(self, style):
+        self.style = style
+    
+    def __init__(self, speed):
+        self.speed = speed
 
+class Car(Vehicle):
+    def __init__(self, engine):
+        super().__init__("Car")
+        self.wheels = 4
+        self.doors = 4
+        self.engine = engine
+
+Car1 = Car("Gas")
+Car2 = Car("Electric")
+
+print(Car1.engine)
+print(Car2.engine)
+
+print(Car1.doors)
+print(Car2.doors)
+
+
+
+ 
